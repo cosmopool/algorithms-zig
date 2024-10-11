4ef34bdf7716e0e06ad9de38bd1091bd6d278918
Kaio Delphino <dash.zet@gmail.com>
2024-10-11 17:40:51 -0300

    wip

--- src/insertion_sort.zig
+++ src/insertion_sort.zig
@@ -1,33 +1,34 @@
 const std = @import("std");
 const testing = std.testing;
+const testAllocator = std.testing.allocator;
 
 // INSERT SORT ALGORITHM
 // Best fit for small sets of data
 
-fn sort(comptime T: type, allocator: std.mem.Allocator, input: []const T, toAdd: T) std.mem.Allocator.Error![]const T {
-    var output: []T = try allocator.alloc(T, input.len + 1);
+fn sort(comptime T: type, allocator: std.mem.Allocator, comptime input: []const T) std.mem.Allocator.Error![]const T {
+    const output: []T = try allocator.alloc(T, input.len);
     std.mem.copyForwards(T, output, input);
-    output[input.len] = toAdd;
 
     for (1..output.len) |i| {
         const key = output[i];
-        var j = i - 1;
-        while (j > 0 and output[j] > key) {
-            output[j + 1] = output[j];
-            j = j - 1;
-            output[j] = key;
+        var j: usize = i - 1;
+        while (j >= 0 and output[j] > key) {
+        //     output[j + 1] = output[j];
+        //     if (j == 0) break;
+        //     j = j - 1;
         }
+        // output[j + 1] = key;
     }
 
     return output;
 }
 
-test "sort 3 items array" {
+test "sort 2 items array" {
     const inputSet = [_]u8{ 2, 1 };
-    const expected = [_]u8{ 1, 2, 3 };
-    const result = try sort(u8, std.testing.allocator, inputSet[0..], 3) catch |err| {
-        std.debug.print("error: {}\n", .{err});
-    };
+    const expected = [_]u8{ 1, 2 };
+
+    const result = try sort(u8, testAllocator, inputSet[0..]);
+    defer testAllocator.free(result);
 
     try testing.expect(std.mem.eql(u8, result, expected[0..]));
 }
